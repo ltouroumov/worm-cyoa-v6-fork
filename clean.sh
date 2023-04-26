d906f4e40d8e7d98c4a9f4eaa7e8e41f7b46dc4e
Laureline David <ltouroumov@gmail.com>
2023-04-26 19:35:37 +0200

    V17 - SO MANY T1 POWERS 😍 (by Symbol)

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 # Apply fix patches
-python3 -m cyoa.tools.client project.patch --project $1 \
+python3 -m cyoa.tools.client project.patch --project "$1" \
   --patch cyoa.patch:FixScoreLabels \
           cyoa.patch:FixActiveFlags \
           cyoa.patch:FixConditionLabels \
